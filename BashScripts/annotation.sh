bb4edd4bcd578f6e5cc25b23ee64b5dcd1c15969
Michael Landi <47971974+LandiMi2@users.noreply.github.com>
2023-08-02 16:51:43 +0300

    Update annotation.sh

--- BashScripts/annotation.sh
+++ BashScripts/annotation.sh
@@ -3,7 +3,6 @@
 #Transposable elements annotation by EDTA 
 EDTA.pl --genome asm.hap1.fasta --overwrite 1 --sensitive 1 --anno 1 --evaluate 1 --threads 32 2> edta.log &
 
-#filtering of the EDTA output using Rscript (find the script in the Rscript folder)
 
 #Gene annotation using the funannotate tool
 #!/usr/bin/bash -l
